a6fb4c42c7e5747886392274fc0a94b3a4071fcb
marioncalpe <marioncalpe2a@gmail.com>
2023-07-27 20:19:48 +0200

    Sidenav
    Ajout du logo main
    ajout des logo de naviguation
    ajout du copyright

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import 'boxicons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'portfolio';
+  title = 'Portfolio';
 }
