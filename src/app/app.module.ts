0a0424df06ffb24e96ea40fd507fc2d9d17b04e5
marion calpena <marioncalpe2a@gmail.com>
2023-09-03 11:26:05 +0200

    changement fond projet portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'; // Import other necessary Angular Material modules if you use them.
-
+import {MatTooltipModule} from '@angular/material/tooltip';
 import { AppComponent } from './app.component';
 import { ModalComponent } from './modal/modal.component';
 
@@ -17,6 +17,7 @@ import { ModalComponent } from './modal/modal.component';
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule, // Add other necessary Angular Material modules to the 'imports' array.
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
