d85754e081810e387aa180979014b9e2b26bdc17
marion calpena <marioncalpe2a@gmail.com>
2024-02-27 10:47:39 +0100

    ajout du footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, ElementRef, HostListener, Inject, OnInit } from '@angular/core';
 
 import 'boxicons';
 @Component({
@@ -7,6 +8,7 @@ import 'boxicons';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit{
+  // windowScrolled!: boolean;
   title = 'Portfolio';
   showModal: boolean = false;
   isPanelOpen = false;
@@ -18,7 +20,7 @@ export class AppComponent implements OnInit{
     this.showModal = false;
   }
 
-  constructor(private elementRef: ElementRef) {}
+  constructor(private elementRef: ElementRef, @Inject(DOCUMENT) private document: Document) {}
 
   on() {
     this.elementRef.nativeElement.querySelector("#overlay").style.display = "block";
@@ -40,4 +42,33 @@ export class AppComponent implements OnInit{
     event.stopPropagation();
     this.modal.classroom.status = true;
   }
+
+  // @HostListener('window:scroll', [])
+  // onWindowScroll() {
+  //   if (
+  //     window.scrollY ||
+  //     document.documentElement.scrollTop ||
+  //     document.body.scrollTop > 100
+  //   ) {
+  //     this.windowScrolled = true;
+  //   } else if (
+  //     (this.windowScrolled && window.scrollY) ||
+  //     document.documentElement.scrollTop ||
+  //     document.body.scrollTop < 10
+  //   ) {
+  //     this.windowScrolled = false;
+  //   }
+  // }
+
+  // scrollToTop() {
+  //   (function smoothscroll() {
+  //     let currentScroll =
+  //       document.documentElement.scrollTop || document.body.scrollTop;
+
+  //     if (currentScroll > 0) {
+  //       window.requestAnimationFrame(smoothscroll);
+  //       window.scrollTo(0, currentScroll - currentScroll / 8);
+  //     }
+  //   })();
+  // }
 }
